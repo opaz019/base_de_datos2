6e3ef58b2858b37b8eb463ccf5795077eff4c917
orlando <orlandopaz645@gmail.com>
2020-11-19 20:09:58 -0300

    preuba bd2

--- 3_consultas.sql
+++ 3_consultas.sql
@@ -1,13 +1,16 @@
 #¿Qué cliente realizó la compra más cara?
 
-prueba=# SELECT cliente.nombre_cliente, factura.subtotal FROM cliente INNER JOIN factura ON cliente.id_cliente = factura.id_cliente ORDER BY factura.subtotal DESC LIMIT 1;
+prueba=# SELECT cliente.nombre_cliente, factura.subtotal FROM cliente INNER JOIN factura ON cliente.id_cliente = factura.id_cliente 
+where 
+
+ORDER BY factura.subtotal DESC LIMIT 1;
  nombre_cliente | subtotal
 ----------------+----------
  orlando        |     1300
 (1 fila)
 
 #¿Qué cliente pagó sobre 100 de monto?
-prueba=# SELECT distinct cliente.nombre_cliente, factura.subtotal FROM cliente INNER JOIN factura ON cliente.id_cliente = factura.id_cliente WHERE factura.subtotal > 100;
+prueba=# SELECT distinct cliente.nombre_cliente FROM cliente INNER JOIN factura ON cliente.id_cliente = factura.id_cliente WHERE factura.subtotal > 100;
  nombre_cliente | subtotal
 ----------------+----------
  gloria         |     1100
@@ -23,7 +26,7 @@ prueba=# SELECT distinct cliente.nombre_cliente, factura.subtotal FROM cliente I
 
 
 #¿Cuantos clientes han comprado el producto 6?
-prueba=# SELECT COUNT(cliente.nombre_cliente) FROM cliente INNER JOIN factura ON cliente.id_cliente = factura.id_cliente INNER JOIN factura_producto ON factura.numero_factura = factura_producto.numero_factura WHERE factura_producto.id_producto = 6;
+prueba=# SELECT COUNT(distinct cliente.nombre_cliente) FROM cliente INNER JOIN factura ON cliente.id_cliente = factura.id_cliente INNER JOIN factura_producto ON factura.numero_factura = factura_producto.numero_factura WHERE factura_producto.id_producto = 6;
  count
 -------
      1
